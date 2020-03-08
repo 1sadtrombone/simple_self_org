79e93bdf713d2517fdd88a9ffc66b1422f747349
Taj Dyson <taj.dyson@gmail.com>
2020-03-08 14:48:38 -0400

    Saved movies of biased walker and images of step length walker.

--- dual_walkers_prob.py
+++ dual_walkers_prob.py
@@ -1,26 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+from matplotlib.animation import FuncAnimation, writers
 
 it_max = 100
 
-x = [np.random.uniform()]
-y = [np.random.uniform()]
+x = [np.random.uniform()-1/2]
+y = [np.random.uniform()-1/2]
 
 fig, ax = plt.subplots()
 ln, = plt.plot([], [], 'r-')
 pt, = plt.plot([], [], 'C0.')
 
 def init():
-    ax.set_xlim(-10,150)
-    ax.set_ylim(-10,150)
+    ax.set_xlim(-50,50)
+    ax.set_ylim(-50,50)
     return ln,
 
 def update(frame):
 
     roll = np.random.random(size=2)
-    p_right = y[-1] / np.sqrt(y[-1]**2 + x[-1]**2)
-    p_up = x[-1] / np.sqrt(y[-1]**2 + x[-1]**2)
+    p_right = np.abs(y[-1]) / np.sqrt(y[-1]**2 + x[-1]**2)
+    p_up = np.abs(x[-1]) / np.sqrt(y[-1]**2 + x[-1]**2)
     direction_x = 2*((p_right > roll[0])-1/2)
     direction_y = 2*((p_up > roll[1])-1/2)
     x.append(x[-1] + direction_x)
@@ -31,5 +31,7 @@ def update(frame):
     return ln,
 
 ani = FuncAnimation(fig, update, frames=np.empty(it_max), init_func=init)
+
+ani.save('diag2.mp4')
 
 plt.show()
